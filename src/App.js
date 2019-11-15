eadcb81a6c427588760737535e2f62076812b7d2
ZhiduReborn <zhiduyan@gmail.com>
2019-11-15 11:29:55 -0600

    one more typo

--- src/App.js
+++ src/App.js
@@ -137,9 +137,7 @@ class App extends Component {
               })
             }).then(response => response.json())
               .then(count => {
-                this.setState({user: {
-                  entries: count
-                }})
+                this.setState(Object.assign(this.state.user, {entries: count}))
               })
             this.displayFaceBox(response)
           }
